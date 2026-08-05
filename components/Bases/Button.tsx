a70efeb3a471581dc77a92b622b8716983822168
Joey Wasin <wasin.ww1402@gmail.com>
2026-08-05 17:47:30 +0700

    feat: update button styles component library

--- components/Bases/Button.tsx
+++ components/Bases/Button.tsx
@@ -4,37 +4,80 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "group/button inline-flex shrink-0 items-center justify-center rounded-4xl border border-transparent bg-clip-padding text-sm font-medium whitespace-nowrap transition-all outline-none select-none focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/30 active:not-aria-[haspopup]:translate-y-px disabled:pointer-events-none disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+  "group/button inline-flex shrink-0 font-bold items-center justify-center rounded-lg transition-all",
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/80",
-        outline:
-          "border-border bg-background hover:bg-muted hover:text-foreground aria-expanded:bg-muted aria-expanded:text-foreground dark:bg-transparent dark:hover:bg-input/30",
-        secondary:
-          "bg-secondary text-secondary-foreground hover:bg-[color-mix(in_oklch,var(--secondary),var(--foreground)_5%)] aria-expanded:bg-secondary aria-expanded:text-secondary-foreground",
-        ghost:
-          "hover:bg-muted hover:text-foreground aria-expanded:bg-muted aria-expanded:text-foreground dark:hover:bg-muted/50",
-        destructive:
-          "bg-destructive/10 text-destructive hover:bg-destructive/20 focus-visible:border-destructive/40 focus-visible:ring-destructive/20 dark:bg-destructive/20 dark:hover:bg-destructive/30 dark:focus-visible:ring-destructive/40",
-        link: "text-primary underline-offset-4 hover:underline",
+        contained:
+          "border border-transparent disabled:bg-muted disabled:text-muted-foreground/50 disabled:pointer-events-none",
+        outlined:
+          "border disabled:border-muted disabled:text-muted-foreground/50 disabled:pointer-events-none",
+      },
+      color: {
+        primary: "",
+        accent: "",
+        error: "",
+        gray: "",
       },
       size: {
-        default:
-          "h-9 gap-1.5 px-3 has-data-[icon=inline-end]:pr-2.5 has-data-[icon=inline-start]:pl-2.5",
-        xs: "h-6 gap-1 px-2.5 text-xs has-data-[icon=inline-end]:pr-2 has-data-[icon=inline-start]:pl-2 [&_svg:not([class*='size-'])]:size-3",
-        sm: "h-8 gap-1 px-3 has-data-[icon=inline-end]:pr-2 has-data-[icon=inline-start]:pl-2",
-        lg: "h-10 gap-1.5 px-4 has-data-[icon=inline-end]:pr-3 has-data-[icon=inline-start]:pl-3",
-        icon: "size-9",
-        "icon-xs": "size-6 [&_svg:not([class*='size-'])]:size-3",
-        "icon-sm": "size-8",
-        "icon-lg": "size-10",
+        small: "px-3 py-1 wongnok-text-xs",
+        medium: "px-4.5 py-2 wongnok-text-sm",
+        large: "px-6 py-2 wongnok-text-base",
       },
     },
     defaultVariants: {
-      variant: "default",
-      size: "default",
+      variant: "contained",
+      color: "primary",
+      size: "medium",
     },
+    compoundVariants: [
+      {
+        variant: "contained",
+        color: "primary",
+        className: "bg-primary text-primary-foreground hover:bg-primary/90",
+      },
+      {
+        variant: "contained",
+        color: "accent",
+        className: "bg-accent text-accent-foreground hover:bg-accent/90",
+      },
+      {
+        variant: "contained",
+        color: "error",
+        className:
+          "bg-destructive-strong text-primary-foreground hover:bg-destructive-strong/90",
+      },
+      {
+        variant: "contained",
+        color: "gray",
+        className:
+          "bg-muted-foreground text-primary-foreground hover:bg-muted-foreground/90",
+      },
+      {
+        variant: "outlined",
+        color: "primary",
+        className:
+          "border-primary text-primary hover:border-primary/90 hover:bg-primary/10",
+      },
+      {
+        variant: "outlined",
+        color: "accent",
+        className:
+          "border-accent text-accent hover:border-accent/90 hover:bg-accent/10",
+      },
+      {
+        variant: "outlined",
+        color: "error",
+        className:
+          "border-destructive-strong text-destructive-strong hover:border-destructive-strong/90 hover:bg-destructive-strong/10",
+      },
+      {
+        variant: "outlined",
+        color: "gray",
+        className:
+          "border-muted-foreground text-muted-foreground hover:border-muted-foreground/90 hover:bg-muted-foreground/10",
+      },
+    ],
   },
 );
 
@@ -43,14 +86,15 @@ export type ButtonProps = ButtonPrimitive.Props &
 
 function Button({
   className,
-  variant = "default",
-  size = "default",
+  variant = "contained",
+  size = "medium",
+  color = "primary",
   ...props
 }: ButtonProps) {
   return (
     <ButtonPrimitive
       data-slot="button"
-      className={cn(buttonVariants({ variant, size, className }))}
+      className={cn(buttonVariants({ variant, size, color, className }))}
       {...props}
     />
   );
